e37d43b38ae6db09ac3f1d858e4e7b90b667a264
AaryRajpoot <ronit1336@gmail.com>
2025-11-16 17:38:25 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -77,7 +77,7 @@ resource "aws_security_group" "ec2_security_group" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 
